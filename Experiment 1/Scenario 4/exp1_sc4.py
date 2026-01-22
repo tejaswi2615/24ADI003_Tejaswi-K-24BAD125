2d548b69a16d4560b6110ce70375fd0f8bf91b65
Tejaswi K <tejaswi.24ad@kct.ac.in>
2026-01-22 22:17:49 +0530

    Add files via upload

--- Experiment 1/Scenario 4/exp1_sc4.py
+++ Experiment 1/Scenario 4/exp1_sc4.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+print("Tejaswi - 24BAD125")
+df_bank = pd.read_csv("marketing_campaign.csv", sep='\t')
+
+print(df_bank.head())
+print(df_bank.info())
+
+print(df_bank.isnull().sum())
+
+df_bank['Age'] = 2026 - df_bank['Year_Birth']
+
+plt.hist(df_bank['Age'], bins=20)
+plt.title("Customer Age Distribution")
+plt.xlabel("Age")
+plt.ylabel("Count")
+plt.show()
+
+plt.boxplot(df_bank['Income'].dropna())
+plt.title("Income Distribution")
+plt.ylabel("Income")
+plt.show()
