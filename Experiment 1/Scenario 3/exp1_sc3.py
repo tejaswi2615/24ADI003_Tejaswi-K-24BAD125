a6d55222fbe7aff662562853016e0177311f69ad
Tejaswi K <tejaswi.24ad@kct.ac.in>
2026-01-22 22:14:26 +0530

    Add files via upload

--- Experiment 1/Scenario 3/exp1_sc3.py
+++ Experiment 1/Scenario 3/exp1_sc3.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+print("TejaswiK - 24BAD125")
+df_house = pd.read_csv("housing.csv")
+
+print(df_house.head())
+print(df_house.info())
+
+print(df_house.isnull().sum())
+
+plt.scatter(df_house['area'], df_house['price'])
+plt.title("Area vs Price")
+plt.xlabel("Area")
+plt.ylabel("Price")
+plt.show()
+
+plt.figure(figsize=(8,6))
+sns.heatmap(df_house.corr(), annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap")
+plt.show()
