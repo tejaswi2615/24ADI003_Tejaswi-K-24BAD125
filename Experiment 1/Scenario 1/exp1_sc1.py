72ccebf16a7d882ea36db8b9c7d7087bfab86d93
Tejaswi K <tejaswi.24ad@kct.ac.in>
2026-01-22 22:12:11 +0530

    Update exp1_sc1.py

--- Experiment 1/Scenario 1/exp1_sc1.py
+++ Experiment 1/Scenario 1/exp1_sc1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 
-print("Mithra Ravi- 24BAD070")     
+print("Tejaswi K- 24BAD125")     
 # Load dataset
 df_ecom = pd.read_csv("data.csv", encoding="ISO-8859-1")
 
@@ -34,5 +34,6 @@ plt.title("Top 10 Products by Sales")
 plt.xlabel("Product")
 plt.ylabel("Sales")
 plt.show()
+
 
 
