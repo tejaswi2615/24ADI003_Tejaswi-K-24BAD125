1a0d90d69c33219e447a390fed1bffc725e1bd1a
Tejaswi K <tejaswi.24ad@kct.ac.in>
2026-01-22 21:48:49 +0530

    Add files via upload

--- Experiment 1/Scenario 2/exp1_sc2.py
+++ Experiment 1/Scenario 2/exp1_sc2.py
@@ -1,1 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+print("mithra Ravi-24BAD070")
+df_health = pd.read_csv("diabetes.csv")
+
+#Inspect dataset
+print(df_health.head())
+print(df_health.info())
+
+#missing values
+print(df_health.isnull().sum())
+
+#Glucose levels
+plt.hist(df_health['Glucose'], bins=20)
+plt.title("Glucose Level Distribution")
+plt.xlabel("Glucose")
+plt.ylabel("Frequency")
+plt.show()
+
+#Age
+plt.boxplot(df_health['Age'])
+plt.title("Age Distribution")
+plt.ylabel("Age")
+plt.show()
